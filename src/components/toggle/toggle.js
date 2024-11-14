c44a0e04ee22126d00af6f076dea371e24162630
Artem Zolin <artem.zolin1@yandex.ru>
2024-11-14 08:48:51 +0300

    fin verstka

--- src/components/toggle/toggle.js
+++ src/components/toggle/toggle.js
@@ -5,7 +5,7 @@ import './toggle.css'
 function Toggle () {
   return(
     <div className="toggle">
-      <div className="cheapest toggle-button">САМЫЙ ДЕШЕВЫЙ</div>
+      <div className="cheapest toggle-button toggle-button-active">САМЫЙ ДЕШЕВЫЙ</div>
       <div className="fastest toggle-button">САМЫЙ БЫСТРЫЙ</div>
       <div className="optimal toggle-button">ОПТИМАЛЬНЫЙ</div>
     </div>
